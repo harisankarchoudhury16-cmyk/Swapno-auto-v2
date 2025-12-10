edbe8902ebc8a1209cb96bf83ab926c903aaab19
harisankarchoudhury16-cmyk <harisankarchoudhury16@gmail.com>
2025-12-10 11:44:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/@Graduate_Request_Pro')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Graduate_Request_Pro')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/graduate_movies')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '@Gm_Support_chat') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Gm_Support_chat') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/yours_swapno')
 
 # True Or False
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
